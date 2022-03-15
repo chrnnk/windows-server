71baf1c129b405cee0914fe4c378e1dcd51bc40a
chronnick <87616823+chronnick@users.noreply.github.com>
2022-03-15 15:40:01 -0400

    Update Find-AD-Users-with-Inheritance-dosabled.ps1

--- Find-AD-Users-with-Inheritance-dosabled.ps1
+++ Find-AD-Users-with-Inheritance-dosabled.ps1
@@ -1,1 +1,1 @@
-Get-ADUser -SearchBase "DC=trinity,DC=local" -Filter * -Properties nTSecurityDescriptor | ?{ $_.nTSecurityDescriptor.AreAccessRulesProtected -eq "True" } | ft SamAccountName,Name -AutoSize
+Get-ADUser -SearchBase "DC=butts,DC=local" -Filter * -Properties nTSecurityDescriptor | ?{ $_.nTSecurityDescriptor.AreAccessRulesProtected -eq "True" } | ft SamAccountName,Name -AutoSize
