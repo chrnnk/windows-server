06330c1d2894c87b89b946ce63e1a413d1c052e4
chronnick <87616823+chronnick@users.noreply.github.com>
2022-03-16 00:31:45 -0400

    Delete Find-AD-Users-with-Inheritance-dosabled.ps1

--- Find-AD-Users-with-Inheritance-dosabled.ps1
+++
@@ -1,1 +0,0 @@
-Get-ADUser -SearchBase "DC=butts,DC=local" -Filter * -Properties nTSecurityDescriptor | ?{ $_.nTSecurityDescriptor.AreAccessRulesProtected -eq "True" } | ft SamAccountName,Name -AutoSize
