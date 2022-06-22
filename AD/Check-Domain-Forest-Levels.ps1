e7c8d8092362e35887b9003fc3728bd991af274c
Nick Nelson <nicholasadamnelson@gmail.com>
2022-06-22 14:02:10 -0400

    Update Check-Domain-Forest-Levels.ps1

--- AD/Check-Domain-Forest-Levels.ps1
+++ AD/Check-Domain-Forest-Levels.ps1
@@ -1,2 +1,4 @@
 Get-ADDomain | fl Name,DomainMode
 Get-ADForest | fl Name,ForestMode
+Set-ADDomainMode -identity DOMAIN.local -DomainMode Windows2016Domain
+Set-ADForestMode -Identity DOMAIN.local -ForestMode Windows2016Forest
