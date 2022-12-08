7ee728eb216e2a71b36d8e087be1df15031af839
Nick Nelson <nicholasadamnelson@gmail.com>
2022-12-08 09:26:23 -0500

    Update eval-to-standard.ps1
    Signed-off-by: Nick Nelson <nicholasadamnelson@gmail.com>

--- eval-to-standard.ps1
+++ eval-to-standard.ps1
@@ -1,1 +1,6 @@
 DISM /online /Set-Edition:ServerStandard /ProductKey:brrrrrr /AcceptEula
+#
+• Open an elevated command prompt & use this command: (mod note: this is a public KMS client setup key from Microsoft used for version conversion, but not activation!)
+DISM /online /Set-Edition:ServerStandard /ProductKey:N69G4-B89J2-4G8F4-WWYCC-J464C /AcceptEula
+After rebooting, enter your purchased license key in the Activation settings
+https://www.reddit.com/r/sysadmin/comments/giaqt5/heres_the_trick_for_converting_2019_eval_to/
