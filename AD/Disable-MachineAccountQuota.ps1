f18726fa0e049d710e367019742c149e7a064cb7
Nick Nelson <nick@chrxnn.com>
2022-12-18 00:31:33 -0500

    Update Disable-MachineAccountQuota.ps1
    Signed-off-by: Nick Nelson <nick@chrxnn.com>

--- AD/Disable-MachineAccountQuota.ps1
+++ AD/Disable-MachineAccountQuota.ps1
@@ -1,2 +1,9 @@
 Get-ADObject -Identity ((Get-ADDomain).distinguishedname) -Properties ms-DS-MachineAccountQuota
 Set-ADDomain -Identity ((Get-ADDomain).distinguishedname) -Replace @{"ms-DS-MachineAccountQuota"="0"}
+# By Default Domain Controllers Policy
+# Open Group Policy Management Console ( Start > Run > gpmc.msc).
+# Locate Domain Controllers OU and find Default Domain Controllers Policy.
+# Edit Default Domain Controllers Policy.
+# Expand Computer Configuration – Policies – Windows Settings – Security Settings – User Rights Assignment
+# From right pane right click on Add workstations to domain – Properties – Remove Authenticated Users and Add the User or Group that you are delegating domain joining permissions.
+# Click Apply and then OK to close the Properties window. 
