14ec178bb9ff6c2b7928f6cc0bb6b24548705d2a
Nick Nelson <nick@chrxnn.com>
2023-02-08 18:28:06 -0500

    added comments with CSV headers from recent 365 export

--- AD/Import-CSV-Attributes-into-AD.ps1
+++ AD/Import-CSV-Attributes-into-AD.ps1
@@ -1,4 +1,6 @@
 # If you output the CSV from 365, you will need to change the separators in the proxy addresses column from + to , before import
+# Below is the sample header from a 365 export on 2/8/2023
+# SamAccountName,Name,Office,PhoneNumber,proxyAddresses,UserPrincipalName
 $USERS = Import-CSV C:\Temp\UserList.csv
 $USERS | ForEach-Object {
 
