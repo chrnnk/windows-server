5c80ea0f552caccfc3959499e67c485c3b55cda5
Nick Nelson <nick@chrxnn.com>
2023-08-14 13:30:48 -0400

    Create Copy-Files-With-WinSCP.bat
    Signed-off-by: Nick Nelson <nick@chrxnn.com>

---
+++ Copy-Files-With-WinSCP.bat
@@ -0,0 +1,23 @@
+@echo off
+
+"C:\Program Files (x86)\WinSCP\WinSCP.com" ^
+  /log="\\SERVER\WinSCP.log" /ini=nul ^
+  /command ^
+    "open sftp://USERNAME:PASSWORD@HOSTNAME:PORT/ -hostkey=""ssh-ed25519 255 SSHKEY""" ^
+    "lcd ""E:\SourceFolder""" ^
+    "cd /TargetFolder" ^
+    "put FileToUpload.file" ^
+    "exit"
+
+set WINSCP_RESULT=%ERRORLEVEL%
+if %WINSCP_RESULT% equ 0 (
+  echo Success
+) else (
+  exit /b %WINSCP_RESULT%
+)
+
+@Delete source file after upload
+TIMEOUT 3
+DEL "E:\SourceFolder\FileToUpload.file"
+
+exit /b %WINSCP_RESULT%
