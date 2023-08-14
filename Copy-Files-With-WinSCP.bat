25b60900a2771d91eaf9ba2da6243a50a18f861e
Nick Nelson <nick@chrxnn.com>
2023-08-14 13:50:20 -0400

    Update Copy-Files-With-WinSCP.bat
    Signed-off-by: Nick Nelson <nick@chrxnn.com>

--- Copy-Files-With-WinSCP.bat
+++ Copy-Files-With-WinSCP.bat
@@ -21,3 +21,31 @@ TIMEOUT 3
 DEL "E:\SourceFolder\FileToUpload.file"
 
 exit /b %WINSCP_RESULT%
+
+@ Download (get) file from remote destination
+WinSCP.com ^
+  /log="\\SERVER\WinSCP.log" /ini=nul ^
+  /command ^
+    "open sftp://USERNAME:PASSWORD@HOSTNAME:PORT/ -hostkey=""ssh-ed25519 255 SSHKEY""" ^
+    "cd /testout" ^
+    "lcd ""E:\SourceFolder""" ^
+    "get *.dat" ^
+    "rm *.dat" ^
+    "exit"
+
+set WINSCP_RESULT=%ERRORLEVEL%
+if %WINSCP_RESULT% equ 0 (
+  echo Success
+) else (
+  echo Error
+)
+
+TIMEOUT 3
+type nul >"E:\XP.048468961"
+TIMEOUT 2
+for %%f in ("E:\Shared Files\*.dat") do type "%%f" >> "E:\Shared Files\EXP.048468961"
+TIMEOUT 3
+move /Y "E:\Shared Files\*.dat" "E:\Shared Files\Processed"
+TIMEOUT 3
+
+exit /b %WINSCP_RESULT%
