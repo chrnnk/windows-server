8fc9fcbd611781db47dc3b6a9efbccd9787055f3
Nick Nelson <nick@chrnnk.tv>
2024-06-03 13:00:01 -0400

    update domain from chrxnn to chrnnk

--- DNS/Sync-DNS-Zones.ps1
+++ DNS/Sync-DNS-Zones.ps1
@@ -1,2 +1,2 @@
-Sync-DnsServerZone -Name "chrxnn.local" -PassThru -Verbose
+Sync-DnsServerZone -Name "chrnnk.local" -PassThru -Verbose
 <# PassThru - Returns an object representing the item with which you are working. By default, this cmdlet does not generate any output. #>
